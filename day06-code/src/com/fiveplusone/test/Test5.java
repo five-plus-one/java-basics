5b62215fff8f98eaae35332e2ca5bafdaebb0561
five-plus-one <5plus1@five-plus-one.com>
2026-03-04 19:31:02 +0800

    Day06

---
+++ day06-code/src/com/fiveplusone/test/Test5.java
@@ -0,0 +1,21 @@
+package com.fiveplusone.test;
+
+public class Test5 {
+    static void main(String[] args) {
+        int[] arr = {11, 22, 33, 44, 55};
+        printArr(arr);
+    }
+
+    //定义方法用于数组的遍历
+    public static void printArr(int[] arr){
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if(i==arr.length-1){
+                System.out.print(arr[i]);
+            }else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.print("]");
+    }
+}
