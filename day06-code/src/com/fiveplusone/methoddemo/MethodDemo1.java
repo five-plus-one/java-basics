0c98ed18e32f47000f6d11cc8059e578dcc6a822
five-plus-one <5plus1@five-plus-one.com>
2026-03-04 18:49:55 +0800

    Day06

---
+++ day06-code/src/com/fiveplusone/methoddemo/MethodDemo1.java
@@ -0,0 +1,14 @@
+package com.fiveplusone.methoddemo;
+
+public class MethodDemo1 {
+    static void main(String[] args) {
+        // 调用方法
+        playGame();
+        playGame();
+    }
+    public static void playGame(){
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
---
+++ day06-code/src/com/fiveplusone/methoddemo/MethodDemo2.java
@@ -0,0 +1,15 @@
+package com.fiveplusone.methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        // 调用方法
+        getSum();
+    }
+    public static void getSum(){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+
+}
