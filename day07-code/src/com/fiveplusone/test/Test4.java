b09df4fd02036fb099a27c0dffd2f80606eb1452
five-plus-one <5plus1@five-plus-one.com>
2026-03-04 20:33:14 +0800

    Day07

---
+++ day07-code/src/com/fiveplusone/test/Test4.java
@@ -0,0 +1,14 @@
+package com.fiveplusone.test;
+
+public class Test4 {
+    static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] newArr = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            newArr[i]=arr[i];
+        }
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.println(newArr[i]);
+        }
+    }
+}
