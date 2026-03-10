79eeb5feade92a9e0a33054ae29aaf47c3cff497
five-plus-one <5plus1@five-plus-one.com>
2026-03-10 18:48:38 +0800

    day08

---
+++ day08-code/src/com/fiveplusone/test2/StudentTest.java
@@ -0,0 +1,9 @@
+package com.fiveplusone.test2;
+
+public class StudentTest {
+    static void main(String[] args) {
+        Student s = new Student("张三",18);
+        System.out.println(s.getAge());
+        System.out.println(s.getName());
+    }
+}
