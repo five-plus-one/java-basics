d01b7355a7168c5ff5ad28f521b504cc31ca2a59
five-plus-one <5plus1@five-plus-one.com>
2026-03-11 13:52:42 +0800

    feat(day09): add Goods entity class and array demo
    - Add Goods class with id, name, price, count fields and full getters/setters
    - Add GoodsTest to demonstrate object array creation and iteration

---
+++ day09-code/src/com/fiveplusone/test2/GoodsTest.java
@@ -0,0 +1,16 @@
+package com.fiveplusone.test2;
+
+public class GoodsTest {
+    public static void main(String[] args) {
+        Goods[] arr = new Goods[3];
+        Goods g1 = new Goods("001","手机",5999.0,100);
+        Goods g2 = new Goods("002","保温杯",227.0,50);
+        Goods g3 = new Goods("003","枸杞",12.7,70);
+        arr[0] = g1;
+        arr[1] = g2;
+        arr[2] = g3;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].toString());
+        }
+    }
+}
