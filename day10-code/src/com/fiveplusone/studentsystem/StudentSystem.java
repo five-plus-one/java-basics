f4173b79f13aefff2a94e225e290179eef6f312b
five-plus-one <5plus1@five-plus-one.com>
2026-03-13 18:50:28 +0800

    feat(studentsystem): integrate ArrayList for student data management

--- day10-code/src/com/fiveplusone/studentsystem/StudentSystem.java
+++ day10-code/src/com/fiveplusone/studentsystem/StudentSystem.java
@@ -1,9 +1,11 @@
 package com.fiveplusone.studentsystem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentSystem {
     public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
         loop: while (true) {
             System.out.println("--------");
             System.out.println("1.添加");
@@ -15,10 +17,10 @@ public class StudentSystem {
             Scanner sc = new Scanner(System.in);
             String choose = sc.next();
             switch(choose){
-                case "1" -> addStudent();
-                case "2" -> deleteStudent();
-                case "3" -> updateStudent();
-                case "4" -> queryStudent();
+                case "1" -> addStudent(list);
+                case "2" -> deleteStudent(list);
+                case "3" -> updateStudent(list);
+                case "4" -> queryStudent(list);
                 case "5" -> {
                     System.out.println("退出");
 //                    break loop;
@@ -30,22 +32,62 @@ public class StudentSystem {
     }
 
     //添加学生
-    public static void addStudent(){
-        System.out.println("添加");
+    public static void addStudent(ArrayList<Student> list){
+        Scanner sc = new Scanner(System.in);
+        String id = null;
+        while (true) {
+            System.out.println("请输入学生id");
+            id = sc.next();
+            boolean flag = contains(list, id);
+            if (flag) {
+                System.out.println("该id已存在，请重新输入");
+            } else {
+                break;
+            }
+        }
+        System.out.println("请输入学生姓名");
+        String name = sc.next();
+        System.out.println("请输入学生年龄");
+        int age = sc.nextInt();
+        System.out.println("请输入学生家庭住址");
+        String address = sc.next();
+
+        Student stu = new Student(id,name,age,address);
+        list.add(stu);
+        System.out.println("添加成功");
     }
 
     //删除学生
-    public static void deleteStudent(){
+    public static void deleteStudent(ArrayList<Student> list){
         System.out.println("删除");
     }
 
     //修改学生
-    public static void updateStudent(){
+    public static void updateStudent(ArrayList<Student> list){
         System.out.println("修改");
     }
 
     //查询学生
-    public static void queryStudent(){
-        System.out.println("查询");
+    public static void queryStudent(ArrayList<Student> list){
+        if(list.size() == 0){
+            System.out.println("当前无学生信息，请添加后查询");
+            return;
+        }
+        System.out.println("id\t姓名\t年龄\t家庭住址");
+        for (int i = 0; i < list.size(); i++) {
+            Student stu = list.get(i);
+            System.out.println(stu.getId() + "\t" + stu.getName() + "\t" + stu.getAge() + "\t" + stu.getAddress());
+        }
+    }
+
+    //判断id在集合中是否存在
+    public static boolean contains(ArrayList<Student> list,String id){
+        for (int i = 0; i < list.size(); i++) {
+            Student stu = list.get(i);
+            if(stu.getId().equals(id)){
+                return true;
+            }
+        }
+        return false;
     }
 }
