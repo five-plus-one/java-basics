6d4323ea01071dcfaf295b3c5d01eebed6f7d744
five-plus-one <5plus1@five-plus-one.com>
2026-03-13 19:06:16 +0800

    refactor(studentsystem): normalize whitespace and formatting in StudentSystem

--- day10-code/src/com/fiveplusone/studentsystem/StudentSystem.java
+++ day10-code/src/com/fiveplusone/studentsystem/StudentSystem.java
@@ -59,12 +59,39 @@ public class StudentSystem {
 
     //删除学生
     public static void deleteStudent(ArrayList<Student> list){
-        System.out.println("删除");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要删除的id");
+        String id = sc.next();
+        int index = getIndex(list,id);
+        if(index >= 0){
+            list.remove(index);
+            System.out.println("删除成功");
+        }else{
+            System.out.println("该id不存在");
+        }
     }
 
     //修改学生
     public static void updateStudent(ArrayList<Student> list){
-        System.out.println("修改");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要啊修改的学生id");
+        String id = sc.next();
+        int index = getIndex(list,id);
+        if(index >= 0){
+            Student stu = list.get(index);
+            System.out.println("请输入新的姓名");
+            String newName = sc.next();
+            stu.setName(newName);
+            System.out.println("请输入新的年龄");
+            int newAge = sc.nextInt();
+            stu.setAge(newAge);
+            System.out.println("请输入新的家庭住址");
+            String newAddress = sc.next();
+            stu.setAddress(newAddress);
+            System.out.println("修改成功");
+        }else{
+            System.out.println("该id不存在");
+        }
     }
 
     //查询学生
@@ -82,12 +109,16 @@ public class StudentSystem {
 
     //判断id在集合中是否存在
     public static boolean contains(ArrayList<Student> list,String id){
+        return getIndex(list,id) >= 0;
+    }
+
+    public static int getIndex(ArrayList<Student> list,String id){
         for (int i = 0; i < list.size(); i++) {
             Student stu = list.get(i);
             if(stu.getId().equals(id)){
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 }
