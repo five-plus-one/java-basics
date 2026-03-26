311272ebcf60a3415a8490c363058f71580f323a
five-plus-one <5plus1@five-plus-one.com>
2026-03-26 22:36:54 +0800

    feat(day15): add puzzle game image assets and UI improvements
    - Add 8 animal puzzle image sets (16 pieces each)
    - Add 13 girl puzzle image sets (16 pieces each)
    - Add 10 sport puzzle image sets (16 pieces each)
    - Add login/register UI assets with Chinese labels
    - Update GameJFrame, LoginJFrame, and RegisterJFrame implementations
    - Remove Test.java file

--- day15-code/src/com/fiveplusone/ui/GameJFrame.java
+++ day15-code/src/com/fiveplusone/ui/GameJFrame.java
@@ -4,12 +4,65 @@ import javax.swing.*;
 
 public class GameJFrame extends JFrame {
     public GameJFrame() {
+        initJFrame();
+        initJMenuBar();
+        initImage();
+
+
+
+        //展示界面
+        this.setVisible(true);
+    }
+
+    private void initImage() {
+        //初始化图片
+        ImageIcon icon = new ImageIcon("D:\\5plus1\\Study\\College\\java\\java_basics\\basic-code\\day15-code\\image\\animal\\animal3\\1.jpg");
+
+        JLabel jLabel = new JLabel(icon);
+        jLabel.setBounds(0,0,105,105);
+
+        this.getContentPane().add(jLabel);
+    }
+
+    private void initJMenuBar() {
+        //初始化菜单
+        //创建菜单对象
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu functionJMenu = new JMenu("功能");
+        JMenu aboutJMenu = new JMenu("关于我们");
+
+        //创建选项下面的条目
+        JMenuItem replayItem = new JMenuItem("重新游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登录");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem = new JMenuItem("公众号");
+
+        //将每一个选项下面的条目添加到选项中
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        //添加到菜单中
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initJFrame() {
         this.setSize(603, 680);
         this.setTitle("拼图 v1.0");
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         //设置关闭模式
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setVisible(true);
+
+        //取消默认的居中放置
+        this.setLayout(null);
     }
 }
