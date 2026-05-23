0b3c4346fa823fcdfb1300f9ca2c0a2788450f46
five-plus-one <5plus1@five-plus-one.com>
2026-05-23 20:12:39 +0800

    feat(day17): add producer-consumer example using ArrayBlockingQueue
    - Implement Cook thread as producer that puts food items into queue
    - Implement Foodie thread as consumer that takes food items from queue
    - Demonstrate thread synchronization with bounded queue capacity of 1

---
+++ day17-code/src/com/fiveplusone/b05waitandnotify/ThreadDemo.java
@@ -0,0 +1,14 @@
+package com.fiveplusone.b05waitandnotify;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(1);
+        Cook c = new Cook(queue);
+        Foodie f = new Foodie(queue);
+
+        c.start();
+        f.start();
+    }
+}
